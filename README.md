ff5b41eefacc3a6fa3ffe6ab7426b921f5c7f47b
Schaum7 <falk.altrock@gmail.com>
2020-11-11 17:47:37 +0100

    edited readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# SpritPreisTracker 
+# SpritPreisTracker
+
+added the bat to windows task scheduler to start it whenever i logon
--- benzinpreis_scraper.bat
+++ benzinpreis_scraper.bat
@@ -1,4 +1,3 @@
 @echo off 
 
 C:\Users\Falk\Workspace\SpritPreisTracker\venv\Scripts\python.exe C:\Users\Falk\Workspace\SpritPreisTracker\benzin_scraper.py
-pause
